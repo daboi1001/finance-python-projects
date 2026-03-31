b564ccf6b2046db4d1030a826884c2c44b8dd42f
daboi1001 <arnav.aggarwal1001@gmail.com>
2026-03-31 15:27:11 -0400

    PY4E 9.4 completed

--- PY4E_9_4.py
+++ PY4E_9_4.py
@@ -1,4 +1,5 @@
 emailList =[]
+counts ={}
 fname = input("Enter file name: ")
 if len(fname) < 1:
     fname = "mbox-short.txt"
@@ -10,9 +11,18 @@ for line in fh:
         temp = list(line.split())
         #print(temp)
         emailList.append(temp[1])
-
-#print(emailList)
-count = len(emailList)
+        
 for email in emailList:
-    print(email)
-print("There were", count, "lines in the file with From as the first word")
+    counts[email] = counts.get(email,0)+1
+
+#print(counts)
+
+commonEmail = None
+commonEmailCount = 0
+
+for email, count in counts.items():
+    if commonEmail is None or count > commonEmailCount:
+        commonEmail = email
+        commonEmailCount = count
+
+print(commonEmail, commonEmailCount)
